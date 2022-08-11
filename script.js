e7f24e8eed08ea7bfb0ac83ce7d762422e763a88
syerm88 <komatsuya0709@gmail.com>
2022-08-11 17:10:58 +0900

    ハンバガーメニューリサイズ追加

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ $(function () {
   $('.c-button').on('click', function () { 
     $(this).toggleClass('is-open');
     $('.l-sidebar').toggleClass('is-open');
-    $('.hamburger-demo-cover').toggleClass('is-open'); 
+    $('.hamburger-bg-cover').toggleClass('is-open'); 
   });
+});
+
+$(window).on('resize', function() {
+  let $windowWidth = window.innerWidth;
+
+  if($windowWidth > 1200){
+    $('.c-button').removeClass('is-open');
+    $('.l-sidebar').removeClass('is-open');
+    $('.hamburger-bg-cover').removeClass('is-open');    
+  };
 });
